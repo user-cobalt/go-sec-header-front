10057539fd553fdf36bee1f261ab83856b7351b2
user-cobalt <jt.consultingnyc@gmail.com>
2026-07-13 09:30:37 -0400

    ADDITION: Fix generate-env

--- generate-env.js
+++ generate-env.js
@@ -1,9 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 
+console.log('Current directory:', process.cwd());
+console.log('API_URL:', process.env.API_URL ? 'set' : 'NOT SET');
+console.log('API_KEY:', process.env.API_KEY ? 'set' : 'NOT SET');
+
 const dir = './src/environments';
+console.log('Directory exists:', fs.existsSync(dir));
+
 if (!fs.existsSync(dir)) {
+  console.log('Creating directory...');
   fs.mkdirSync(dir, { recursive: true });
+  console.log('Directory created:', fs.existsSync(dir));
 }
 
 const envDev = `export const environment = {
