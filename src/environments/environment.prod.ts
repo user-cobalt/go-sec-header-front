22c6378390ea042be3c0882375b9bc4a1b2338f1
user-cobalt <jt.consultingnyc@gmail.com>
2026-07-13 15:34:22 -0400

    fixes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: '/api',
+  apiUrl: 'https://go-sec-headers-production.up.railway.app',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: 'https://go-sec-headers-production.up.railway.app',
   apiUrl: '/api',
 
 };
